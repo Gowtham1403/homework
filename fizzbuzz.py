11e35737659ce153bc81734ab7bdd08482b5e33e
Gowtham1403 <gowthamraj1403@gmail.com>
2023-04-16 20:28:54 +0530

    update fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzBuzz(num):
+    if num % 3 == 0 and num % 5 == 0 :
+        return "fizzBuzz"
+    elif num % 3 == 0:
+        return "fizz"
+    elif num % 5 == 0:
+        return "buzz"
+    else:
+        return str(num)
+for i in range (0,51):
+    print(fizzBuzz(i))
