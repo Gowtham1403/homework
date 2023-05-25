241c3166f34e46169fff8f7c8468a5b7069f2e03
Gowtham1403 <gowthamraj1403@gmail.com>
2023-05-25 13:01:49 +0530

    corrected

--- New prblems/forloopAvg.py
+++ New prblems/forloopAvg.py
@@ -9,3 +9,14 @@ for i in range(5):
     total += mark
     avg = total / 5
 print (f"Average mark is: {avg}")
+
+'''
+Output:
+    
+Enter the mark of student1 : 80
+Enter the mark of student2 : 100
+Enter the mark of student3 : 80
+Enter the mark of student4 : 100
+Enter the mark of student5 : 100
+Average mark is: 92.0
+'''
